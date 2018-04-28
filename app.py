ec74547aa54e56c66aaa58897cdd552ce2f44238
Calin Ilie <colinx05@gmail.com>
2018-04-28 18:31:10 +0100

    Login?

--- app.py
+++ app.py
@@ -2,14 +2,20 @@
 
 from flask import Flask
 from flask import render_template
+import credentials
+import spotify_connect
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello world!"
+# spotify_connect = SpotifyConnect(credentials.spotify['user'], credentials.spotify['client_id'], credentials.spotify['client_secret'])
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+
+import json
+
+@app.route('/login')
+def login():
+    return render_template('login.html')

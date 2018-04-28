7f3de22364a68e880a3ffa6dcb2974ed331f5e43
harbinger22 <bogdan.serea@student.manchester.ac.uk>
2018-04-28 23:55:28 +0100

    token_gen now prints scopes

--- token_gen.py
+++ token_gen.py
@@ -9,10 +9,19 @@ import sys
 import spotipy
 import spotipy.util as util
 
+def print_scopes():
+    print("Scopes:")
+    f = open("spotipy.scope")
+    line = f.readline()
+    while line!='':
+        print '\t' + line.strip()
+        line = f.readline()
+
 if(len(sys.argv)==2):
     scope = sys.argv[1]
 else:
     print("Usage: token_gen.py scope")
+    print_scopes()
     sys.exit()
 
 #set up login credentials
@@ -20,12 +29,6 @@ CODRIN_CLIENT_ID = '4616f84bd6c344d49c4d49712de27d1d'
 CODRIN_CLIENT_SECRET = '78fd807f7f06423b9d51f1e1fb4b8df3'
 
 token = util.prompt_for_user_token('',scope,client_id=CODRIN_CLIENT_ID,client_secret=CODRIN_CLIENT_SECRET,redirect_uri='http://localhost:8181/')
-
-
-
-
-
-
 
 out_file = open("token.txt","w")
 out_file.write(token)

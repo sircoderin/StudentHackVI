aabb6e2a03341b1e2883cbf6a6f24312ee0e8630
Codrin Sirboiu <mbaxacs2@student.manchester.ac.uk>
2018-04-28 20:45:24 +0100

    Add artist name to search

--- spotifyClient.py
+++ spotifyClient.py
@@ -9,19 +9,21 @@ from spotipy_utils import *
 
 class Item(object):
     track_name = ""
+    artist_name = ""
     album_name = ""
     track_id = ""
     image_url = ""
 
     # The class "constructor" - It's actually an initializer
-    def __init__(self, track_name, album_name, track_id, image_url):
+    def __init__(self, track_name, artist_name, album_name, track_id, image_url):
         self.track_name = track_name
+        self.artist_name = artist_name
         self.album_name = album_name
         self.track_id = track_id
         self.image_url = image_url
 
-def make_item(track_name, album_name, track_id, image_url):
-    item = Item(track_name, album_name, track_id, image_url)
+def make_item(track_name, artist_name, album_name, track_id, image_url):
+    item = Item(track_name, artist_name, album_name, track_id, image_url)
     return item
 
 
@@ -37,7 +39,7 @@ def search_track(param):
     response = json.loads(response)
     output = []
     for item in response['tracks']['items']:
-        newObj = make_item(item['name'], item['album']['name'], item['id'], item['album']['images'][2]['url'])
+        newObj = make_item(item['name'], item['album']['artists'][0]['name'], item['album']['name'], item['id'], item['album']['images'][2]['url'])
         output.append(newObj)
         #print("%s from album %s with id %s" % (item['name'], item['album']['name'], item['id']))
         #print(item['album']['images'][2]['url'])
@@ -49,4 +51,4 @@ def search_track(param):
 if __name__ == "__main__":
     output = search_track("martin garrix")
     for item in output:
-        print ("%s - %s" % (item.track_name, item.track_id))
+        print ("%s by %s - %s" % (item.track_name, item.artist_name, item.track_id))
